a3c2d83aa1108780e937edd9b9e2877429d84801
yatendra-debug <yatendra@besttoolsmail.online>
2026-04-04 13:48:52 +0530

    Update smtp.js

--- config/smtp.js
+++ config/smtp.js
@@ -3,9 +3,6 @@ const nodemailer = require("nodemailer");
 module.exports = (email, password) => {
   return nodemailer.createTransport({
     service: "gmail",
-    auth: {
-      user: email,
-      pass: password,
-    },
+    auth: { user: email, pass: password }
   });
 };
