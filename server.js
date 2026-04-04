4969a97a44d98d7ae13db58b3029e279a35ead89
yatendra-debug <yatendra@besttoolsmail.online>
2026-04-04 13:48:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,20 +9,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// static frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-// root fix
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "launcher.html"));
 });
 
-// api
 app.use("/send", sendRoute);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server running on", PORT);
-});
+app.listen(PORT, () => console.log("Server running on", PORT));
